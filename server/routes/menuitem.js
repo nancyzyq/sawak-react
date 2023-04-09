eb1ad5cf5b71d8aa9c6681ac5b4b8c48a36bd744
YIQING ZHANG <yiqingzhang@MacBook-Pro.local>
2023-04-09 21:16:23 +1000

    update meuiten controller

--- server/routes/menuitem.js
+++ server/routes/menuitem.js
@@ -1,21 +1,21 @@
-const menuitemController = require('../controllers/menuitemController')
+const menuItemController = require('../controllers/menuItemController')
 const userController = require('../controllers/userController')
 
 function menuitemGetHandler (req, res, next) {
     if (req.query.id) {
-        menuitemController.getMenuitemById(req.query.id).then((menuitem) => {
+        menuItemController.getMenuitemById(req.query.id).then((menuitem) => {
             res.status(200).send(menuitem)
         }).catch((error) => {
             res.status(500).send('err')
         })
     } else if (req.query.type) {
-        menuitemController.getMenuitemByType(req.query.type).then((menuitem) => {
+        menuItemController.getMenuitemByType(req.query.type).then((menuitem) => {
             res.status(200).send(menuitem)
         }).catch((error) => {
             res.status(500).send('err')
         })
     } else {
-        menuitemController.getMenuitems().then((menuitem) => {
+        menuItemController.getMenuitems().then((menuitem) => {
             res.status(200).send(menuitem)
         }).catch((error) => {
             res.status(500).send('err')
@@ -25,7 +25,7 @@ function menuitemGetHandler (req, res, next) {
 
 function menuitemUpdateHandler (req, res, next) {
     // console.log(req.body.menuitem)
-    menuitemController.updateMenuitem(req.body.menuitem).then((menuitem) => {
+    menuItemController.updateMenuitem(req.body.menuitem).then((menuitem) => {
         // console.log(menuitem)
         res.status(200).send(menuitem)
     }).catch((error) => {
@@ -36,7 +36,7 @@ function menuitemUpdateHandler (req, res, next) {
 }
 
 function menuitemCreateHandler (req, res, next) {
-    menuitemController.createMenuitem(req.body.menuitem).then((menuitem) => {
+    menuItemController.createMenuitem(req.body.menuitem).then((menuitem) => {
         res.status(200).send(menuitem)
     }).catch((error) => {
         console.log(error)
@@ -46,7 +46,7 @@ function menuitemCreateHandler (req, res, next) {
 
 function menuitemDeleteHandler (req, res, next) {
     // console.log(req.query.id)
-    menuitemController.deleteMenuitem(req.query.id).then((menuitem) => {
+    menuItemController.deleteMenuitem(req.query.id).then((menuitem) => {
         res.status(200).send(menuitem)
     }).catch((error) => {
         console.log(error)
